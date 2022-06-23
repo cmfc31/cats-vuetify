337bc44a26c47685ef96ff5ac0a5926bccb776ec
Martin Fuentes <cmfc31@gmail.com>
2022-06-23 14:54:38 -0600

    Agregado dialogo de confirmacion

--- src/views/Favoritos.vue
+++ src/views/Favoritos.vue
@@ -1,12 +1,33 @@
 <template>
   <v-container>
+    <v-dialog v-model="dialogEliminar" persistent max-width="500">
+      <v-card>
+        <v-card-title class="text-h5">
+          ¿Esta seguro de remover este favorito?
+        </v-card-title>
+        <v-card-text>Esta accion no es reversible.</v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="red darken-3" text @click="dialogEliminar = false">
+            Cancelar
+          </v-btn>
+          <v-btn color="red darken-3 white--text" @click="removerFavorito">
+            Si, eliminar
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     <v-row wrap>
       <v-col v-for="gato in gatosArray" :key="gato.id" cols="12" sm="6" md="4">
         <v-card class="mx-auto my-2" max-width="374">
           <v-img height="250" :src="gato.image.url"></v-img>
 
           <v-card-actions>
-            <v-btn color="red darken-3" text @click="removerFavorito(gato.id)">
+            <v-btn
+              color="red darken-3"
+              text
+              @click="mostrarDialogEliminar(gato.id)"
+            >
               Remover
             </v-btn>
           </v-card-actions>
@@ -21,6 +42,8 @@ import axios from "axios";
 export default {
   data() {
     return {
+      gatoId: 0,
+      dialogEliminar: false,
       gatosArray: [],
     };
   },
@@ -38,9 +61,13 @@ export default {
           console.log(error);
         });
     },
-    removerFavorito(id) {
+    mostrarDialogEliminar(id) {
+      this.dialogEliminar = true;
+      this.gatoId = id;
+    },
+    removerFavorito() {
       axios
-        .delete(`favourites/${id}`)
+        .delete(`favourites/${this.gatoId}`)
         .then(() => {
           this.$store.dispatch("showSnack", {
             message: "Imagen removida de favoritos.",
